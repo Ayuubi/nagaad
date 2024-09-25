47337db6d0d632ad308d08beca730c08f9366e9f
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-09-25 17:21:09 +0300

    Update __manifest__.py
    Fore upgrade issue by author change

--- nagaad_addons/idil/__manifest__.py
+++ nagaad_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'conf',
+    'author': 'nagad',
 
     'assets': {
         'web.assets_common': [
