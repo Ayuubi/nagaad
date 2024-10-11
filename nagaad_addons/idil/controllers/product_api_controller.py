96c37ff6005fafd86419093df04b161a927494d8
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-10-11 10:00:45 -0700

    Update product_api_controller.py
    commenting the image part

--- nagaad_addons/idil/controllers/product_api_controller.py
+++ nagaad_addons/idil/controllers/product_api_controller.py
@@ -17,7 +17,7 @@ class ProductAPIController(http.Controller):
                     'title': product.name,  # 'name' field from your model
                     'price': product.sale_price,  # 'sale_price' from your model
                     'category': product.category_id.name if product.category_id else '',  # 'category_id' from your model
-                    'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,  
+                    #'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,  
                     'available_in_pos': product.available_in_pos,  # 'available_in_pos' from your model
                     'uom': product.uom_id.name if product.uom_id else ''  # 'uom_id' field from your model
                 })
@@ -38,7 +38,7 @@ class ProductAPIController(http.Controller):
                 'title': product.name,
                 'price': product.sale_price,
                 'category': product.category_id.name if product.category_id else '',
-                'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,
+                #'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,
                 'available_in_pos': product.available_in_pos,
                 'uom': product.uom_id.name if product.uom_id else ''
             }
