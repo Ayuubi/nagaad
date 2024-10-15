567b9087c4c6bc08a1235a4ddf33c0f06caef34d
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-10-15 02:58:03 -0700

    Update product_api_controller.py
    serializing the product category

--- nagaad_addons/idil/controllers/product_api_controller.py
+++ nagaad_addons/idil/controllers/product_api_controller.py
@@ -12,13 +12,14 @@ class ProductAPIController(http.Controller):
             products = request.env['my_product.product'].sudo().search([])
             products_data = []
             for product in products:
+                pos_categories = product.pos_categ_ids.mapped('name')  # Serialize 'pos_categ_ids'
                 products_data.append({
                     'id': product.id,
                     'title': product.name,  # 'name' field from your model
                     'price': product.sale_price,  # 'sale_price' from your model
-                    'Type' : product.pos_categ_ids,
+                    'Type' : pos_categories,  # Serialized 'pos_categ_ids'
                     'category': product.category_id.name if product.category_id else '',  # 'category_id' from your model
-                    #'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,  
+                    # 'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,  
                     'available_in_pos': product.available_in_pos,  # 'available_in_pos' from your model
                     'uom': product.uom_id.name if product.uom_id else ''  # 'uom_id' field from your model
                 })
@@ -34,12 +35,14 @@ class ProductAPIController(http.Controller):
             if not product.exists():
                 return Response(json.dumps({'error': 'Product not found'}), status=404, content_type='application/json')
 
+            pos_categories = product.pos_categ_ids.mapped('name')  # Serialize 'pos_categ_ids'
             product_data = {
                 'id': product.id,
                 'title': product.name,
                 'price': product.sale_price,
                 'category': product.category_id.name if product.category_id else '',
-                #'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,
+                # 'thumbnail': f"data:image/png;base64,{base64.b64encode(product.image_1920).decode('utf-8')}" if product.image_1920 else None,
+                'Type': pos_categories,  # Serialized 'pos_categ_ids'
                 'available_in_pos': product.available_in_pos,
                 'uom': product.uom_id.name if product.uom_id else ''
             }
