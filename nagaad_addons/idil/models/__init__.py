f15d94c70ea1d7efe4e8718382dd52b05f2a3eef
engmdc <ayoubdahir10@gmail.com>
2025-01-06 10:00:11 +0530

    Purchase vendor statement report

--- nagaad_addons/idil/models/__init__.py
+++ nagaad_addons/idil/models/__init__.py
@@ -43,3 +43,4 @@ from . import Idil_Employee_Salary_Advance
 from . import idil_employee_hrm_report
 from . import idil_employee_hrm_report_department
 from . import EmployeePayrollListwithSalaryAdvance
+from . import vendor_statement
