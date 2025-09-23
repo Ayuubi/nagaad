facccb514775f2a326a60b0df706fa1e4006d997
AyoubDahir <yuubiyuuba@gmail.com>
2025-09-23 19:39:22 +0300

    add company id to customers

--- nagaad_addons/idil/models/customers.py
+++ nagaad_addons/idil/models/customers.py
@@ -31,3 +31,12 @@ class Customer(models.Model):
     #
     # )
     image = fields.Binary(string="Image")
+    # 👇 new field for multi-company
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        required=True,
+        default=lambda self: self.env.company,
+        index=True
+    )
+
