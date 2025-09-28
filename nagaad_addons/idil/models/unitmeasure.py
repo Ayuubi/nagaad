ee780f7627949f8479030e9d6bbb982a652d4b9b
AyoubDahir <yuubiyuuba@gmail.com>
2025-09-28 22:09:36 +0300

    mult comp

--- nagaad_addons/idil/models/unitmeasure.py
+++ nagaad_addons/idil/models/unitmeasure.py
@@ -1,10 +1,29 @@
 from odoo import api, models, fields
 
 
-class unitmeasure(models.Model):
+class UnitMeasure(models.Model):
     _name = 'idil.unit.measure'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Unit of Measure'
 
-    name = fields.Char(string='name', required=True, tracking=True)
-    description = fields.Char(string='description', tracking=True)
+    name = fields.Char(string='Name', required=True, tracking=True)
+    description = fields.Char(string='Description', tracking=True)
+    is_base_uom = fields.Boolean(string='Is Base UOM', default=False, tracking=True)
+    related_uom_ids = fields.One2many(
+        'idil.unit.measure.relation',
+        'base_uom_id',
+        string='Related UOMs'
+    )
+
+
+class UnitMeasureRelation(models.Model):
+    _name = 'idil.unit.measure.relation'
+    _description = 'Unit of Measure Relation'
+
+    base_uom_id = fields.Many2one('idil.unit.measure', string='Base UOM', required=True, ondelete='cascade')
+    related_uom_id = fields.Many2one('idil.unit.measure', string='Related UOM', required=True, ondelete='cascade')
+    conversion_factor = fields.Float(
+        string='Conversion Factor',
+        required=True,
+        help='Factor to convert the related UOM to the base UOM'
+    )
