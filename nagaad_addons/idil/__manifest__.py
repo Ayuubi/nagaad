a1d99aa8b90a3e10d86526e34f9d06ae92cb5f8d
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2025-10-06 11:26:16 +0300

    Update __manifest__.py
    pub

--- nagaad_addons/idil/__manifest__.py
+++ nagaad_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'base', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'oneMore',
+    'author': 'pub',
 
     'assets': {
         'web.assets_common': [
