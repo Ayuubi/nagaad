8d6cf63074f5eacc78db7905cac7224fae593fae
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2025-12-15 18:24:37 +0300

    Update __manifest__.py
    MDC

--- nagaad_addons/idil/__manifest__.py
+++ nagaad_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'base', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'Mohamed Dahir',
+    'author': 'Mohamed',
 
     'assets': {
         'web.assets_common': [
